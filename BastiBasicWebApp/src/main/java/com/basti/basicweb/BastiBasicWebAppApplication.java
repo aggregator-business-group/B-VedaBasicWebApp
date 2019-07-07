63dd97f25bbd493a1a1773899dc5256f781b17a3
Ghatge <ghatgep@avaya.com>
2019-07-07 14:20:49 +0530

    Adding project to github

---
+++ BastiBasicWebApp/src/main/java/com/basti/basicweb/BastiBasicWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.basti.basicweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BastiBasicWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BastiBasicWebAppApplication.class, args);
+	}
+
+}
